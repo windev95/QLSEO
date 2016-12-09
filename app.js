6af429b39eb94a6cdf2196470a2f74b3844d517a
Quoc Toan <Mail.Toan95@gmail.com>
2016-12-09 20:31:55 +0700

    Cập nhật

--- app.js
+++ app.js
@@ -3,38 +3,19 @@ var path = require('path');
 var pool =  require('pg');
 var flash = require('express-flash');
 var favicon = require('serve-favicon');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-var passport = require('passport')
-  , LocalStrategy = require('passport-local').Strategy;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var register = require('./routes/register');
 var login = require('./routes/login');
 var check = require ('./routes/check');
-var checkkq = require ('./routes/checkkq');
-// var check = require ('./routes/check/view');
 var news = require ('./routes/news');
-// var Users = require('./models').Users;
 
 var app = express();
-var Sequelize = require('sequelize');
-var sequelize = new Sequelize('postgres://postgres:qtoan2012@localhost:5432/QLSEO');
-var Users = sequelize.define('Users', {
-  email: {
-    type: Sequelize.STRING
-  },
-  password: {
-    type: Sequelize.STRING
-  },
-  namee: {
-    type: Sequelize.STRING
-  }
-});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -42,32 +23,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // authetication setup
-
-
-passport.serializeUser(function(Users, done) {
-  done(null, Users.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  Users.findOne(id).success(function(Users) { done(null, Users); });
-});
-passport.use(new LocalStrategy({
-    usernameField: 'email'
-  },
-  function(email, password, done) {
-
-    Users.find({ where: { email : email } }).then(function (Users) {
-      if (!Users) {
-        return done(null, false, { message: 'Incorrect email address.' });
-      }
-      if (!Users.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, Users);
-    });
-  }
-));
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -80,8 +35,6 @@ app.use(session({
   saveUninitialized: true,
   resave: true
 }));
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
@@ -89,8 +42,6 @@ app.use('/users', users);
 app.use('/register', register);
 app.use('/login', login);
 app.use('/check', check);
-app.use('/checkkq', checkkq);
-// app.use('/check/view', check);
 app.use('/news', news);
 
 // catch 404 and forward to error handler
--- models/users.js
+++ models/users.js
@@ -2,7 +2,6 @@ var express = require('express');
 var pg = require('pg');
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize('postgres://postgres:20121995@localhost:5432/QLSEO');
-var bcrypt = require('bcrypt-nodejs');
 var app = express();
 
 var Users = sequelize.define('Users', {
