efae77aa6de737a93452977f86eecc865db31495
Quoc Toan <Mail.Toan95@gmail.com>
2016-12-15 16:10:32 +0700

    ...

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // authetication setup
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
